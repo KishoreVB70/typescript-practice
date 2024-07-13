a78e0b462259dfaa4d76cb96e97ff3aebc0e036a
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-13 19:57:33 +0530

    added complex function example

--- object.js
+++ object.js
@@ -1,5 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var user = {
     id: 50,
     name: "something"
 };
 var userArray = [{ id: 40, name: "50" }];
+console.log(user.id);
--- object.ts
+++ object.ts
@@ -12,4 +12,16 @@ type UserArray = Array<User>
 
 const userArray: UserArray = [{id: 40, name:"50"}]
 
+console.log(user.id);
+
+const puckOpp = (name: string, id: number | string): {hi: UserArray, bye: User} => {
+    return {hi: [{id:50, name: "me"}], bye: {id:12, name:"bebe"}}
+
+}
+
+function puck(name: string, id: number | string): string {
+    return "hi"
+
+}
+
 export{}
